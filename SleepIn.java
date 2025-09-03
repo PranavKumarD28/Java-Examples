1c3b7a945cbb865634ce4745c07623a43786c620
PranavKumarD28 <pranavkumard28@gmail.com>
2025-09-03 16:08:03 +0530

    new

--- SleepIn.java
+++ SleepIn.java
@@ -1,7 +1,7 @@
 /*
 The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in 
 */
-public class Demo {
+public class SleepIn {
 
   
   public boolean sleepIn(boolean weekday, boolean vacation) {
@@ -12,7 +12,7 @@ public class Demo {
     return false;
   }
   public static void main(String[] args) {
-        Demo obj = new Demo();
+        SleepIn obj = new SleepIn();
 
         System.out.println(obj.sleepIn(false, false)); // true
         System.out.println(obj.sleepIn(true, false));  // false
